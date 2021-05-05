4c3091992b28bbb3a6e305e0788e8ac8ec4336df
pranav87vns <70517170+pranav87vns@users.noreply.github.com>
2021-05-06 01:50:51 +0530

    Add files via upload

---
+++ Data/MovieData.js
@@ -0,0 +1,5 @@
+// Movie Dataset
+
+const movieData = {  "F001":{"title": "Ran", "code": "regular"},  "F002":{"title": "Trois Couleurs: Bleu", "code":"regular"},  } 
+
+export default movieData;
---
+++ Data/custRecord.js
@@ -0,0 +1,5 @@
+// Customer Record
+
+const custRecord = {    "name": "franklin",    "rentals": [      {"movieID": "F001", "days": 3},      {"movieID": "F002", "days": 1},    ]  }    
+
+export default custRecord;
